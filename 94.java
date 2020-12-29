18ef19551bc671fe3c99152bdaedc4752e112644
malleswarchinta92 <malleswar.chinta@gmail.com>
2020-12-29 23:10:32 +0530

    Binary Tree Inorder Traversal stack

--- 94.java
+++ 94.java
@@ -26,4 +26,23 @@ class Solution {
         list.add(root.val);
         inOrder(root.right, list);
     }
+
+    /*Another approach by stack */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
 }
